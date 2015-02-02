d404c5a4b493ce8d1c7033138f17afdbb4d177ea
ruchiraw <ruchira.wageesha@gmail.com>
2015-02-02 16:48:10 +0530

    merging history

--- components/script-engine/org.jaggeryjs.scriptengine/src/main/java/org/jaggeryjs/scriptengine/EngineConstants.java
+++
@@ -1,9 +0,0 @@
-package org.jaggeryjs.scriptengine;
-
-public class EngineConstants {
-
-    public static final String GLOBAL_OBJECT_NAME = "[GLOBAL]";
-
-    public static final String JAGGERY_CONTEXT = "engine.jaggeryContext";
-
-}
